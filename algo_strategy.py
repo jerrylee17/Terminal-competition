9809b2bf79c23f02236d3e30acb7c80cf9f15703
Jerry Lee <jerrylee1157@gmail.com>
2021-11-13 08:31:10 -0800

    Added prediction

--- algo_strategy.py
+++ algo_strategy.py
@@ -6,6 +6,7 @@ from sys import maxsize
 import json
 
 from gamelib.game_state import GameState
+from gamelib.navigation import ShortestPathFinder
 
 
 """
@@ -69,7 +70,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         game_state.submit_turn()
 
     def hello_world(self, game_state: GameState):
-        turret_pos = [(3, 12), (24, 12), (10, 10), (17, 10)]
+        turret_pos = [(3, 12), (24, 12), (9, 10), (18, 10), (13, 7)]
         wall_pos_1 = [
             [0, 13],
             [1, 13],
@@ -101,7 +102,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         # Redeploy stage
         for pos in turret_pos:
             if game_state.can_spawn(TURRET, pos):
-                gamelib.debug_write('deploying turret at {}'.format(pos))
+                gamelib.debug_write("deploying turret at {}".format(pos))
                 game_state.attempt_spawn(TURRET, pos)
         for pos in wall_pos_1:
             if game_state.can_spawn(WALL, pos):
@@ -109,7 +110,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         for pos in wall_pos_2:
             if game_state.can_spawn(WALL, pos):
                 game_state.attempt_spawn(WALL, pos)
-        
+
         # Repair stage
         for pos in turret_pos:
             game_state.attempt_upgrade(pos)
@@ -118,7 +119,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         for pos in wall_pos_2:
             game_state.attempt_upgrade(pos)
 
-
+        # Attack stage
         left_edges = [
             [0, 13],
             [1, 12],
@@ -157,7 +158,95 @@ class AlgoStrategy(gamelib.AlgoCore):
         right = right_edges[11]
         if game_state.get_resource(1) > 10:
             if game_state.can_spawn(SCOUT, left):
-                game_state.attempt_spawn(SCOUT, left, num=int(game_state.get_resource(1)))
+                game_state.attempt_spawn(
+                    SCOUT, left, num=int(game_state.get_resource(1))
+                )
+
+    def calc_resistance(self, game_state: GameState):
+        left_edges = [
+            [0, 13],
+            [1, 12],
+            [2, 11],
+            [3, 10],
+            [4, 9],
+            [5, 8],
+            [6, 7],
+            [7, 6],
+            [8, 5],
+            [9, 4],
+            [10, 3],
+            [11, 2],
+            [12, 1],
+            [13, 0],
+        ]
+        left_destinations = [
+            [14, 27],
+            [15, 26],
+            [16, 25],
+            [17, 24],
+            [18, 23],
+            [19, 22],
+            [20, 21],
+            [21, 20],
+            [22, 19],
+            [23, 18],
+            [24, 17],
+            [25, 16],
+            [26, 15],
+            [27, 14],
+            [13, 0],
+            [14, 0],
+        ]
+
+        edge_resistance = {}
+        pathfinder = ShortestPathFinder()
+        for pos in left_edges:
+            edge_resistance[pos] = 0
+            path_edges = pathfinder.navigate_multiple_endpoints(
+                pos, left_destinations, game_state)
+            for path in path_edges:
+                # Still on my territory
+                if path < 14:
+                    continue
+                # Calculate value
+                edge_resistance[pos] += len(game_state.get_attackers(path, 0))
+
+    def attack_edge(self, game_state: GameState):
+        left = (13, 0)
+        right = (14, 0)
+
+        # Determine resistance on left
+        left_resistance = 0
+
+        # Determine resistance on right
+        right_resistance = 0
+
+        def attack_left():
+            if game_state.can_spawn(SCOUT, left):
+                game_state.attempt_spawn(
+                    SCOUT, left, num=int(game_state.get_resource(1))
+                )
+                return True
+            return False
+
+        def attack_right():
+            if game_state.can_spawn(SCOUT, right):
+                game_state.attempt_spawn(
+                    SCOUT, right, num=int(game_state.get_resource(1))
+                )
+                return True
+            return False
+
+        if left_resistance < right_resistance:
+            # Attack left
+            if not attack_left():
+                # If attack left fails, attack right
+                attack_right()
+        else:
+            # Attack right
+            if not attack_right():
+                # If attack right fails, attack left
+                attack_left()
 
 
 if __name__ == "__main__":
